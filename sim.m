423d957e9dc5e3bb2f9f13ce5e0321f0d2f4a2bb
Bayesian <kexiongwen@gmail.com>
2023-04-23 18:29:21 +0800

    update

--- sim.m
+++ sim.m
@@ -1,5 +1,5 @@
 tic
-n=100;
+n=500;
 p=1000;
 BetaTrue = zeros(p,1);
 BetaTrue(1)=3;
@@ -12,20 +12,23 @@ BetaTrue(26)=-0.5;
 BetaTrue(31)=2.0;
 BetaTrue(46)=-1.2;
 BetaTrue(51)=-1;
-SigmaTrue=1;
 Corr=0.5.^toeplitz((0:p-1));
 X=mvnrnd(zeros(1,p),Corr,n);
-Y=X*BetaTrue+SigmaTrue.*randn([n 1]);
+Z=binornd(1,0.8,n,1);
+SigmaTrue1=1;
+SigmaTrue2=6;
+Y=X*BetaTrue+(SigmaTrue1*Z+SigmaTrue2*(1-Z)).*randn([n 1]);
 toc
 
-C=3;
+Q=0.5;
+C=0.5;
 s=1;
 
 
 tic
-[beta,sparsity,sigma2]=CD_NSB(Y,X,C,s);
+[beta,sparsity]=SQR(Y,X,Q,C,s);
 toc
 
-L2=norm(beta-BetaTrue);
+[L2,L1,Ham,FDR,FNDR]=metric(beta,BetaTrue);
 
 
